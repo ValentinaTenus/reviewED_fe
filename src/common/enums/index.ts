be78c75e570ef807da15285d203b118711e08532
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-07 00:10:06 +0200

    delete enum

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -2,7 +2,6 @@ export { AppRoute } from './app-route.enum';
 export { ButtonSize } from './button-size.enum';
 export { ButtonType } from './button-types.enum';
 export { ButtonVariant } from './button-variants.enum';
-export { DividerVariant } from './divider-variant.enum';
 export { IconName } from './icon-name.enum';
 export { IconSize } from './icon-size.enum';
 
