7ce8675017d4d4a25fe7f461a3d9bde90ab50e2a
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-06 16:08:32 +0200

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
+import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const indexZero = 0;
+const indexOne = 1;
+
+const config = ({ mode }: ConfigEnv): ReturnType<typeof defineConfig> => {
+	const { VITE_APP_DEVELOPMENT_PORT } = loadEnv(mode, process.cwd());
+
+	return defineConfig({
+		build: {
+			outDir: "dist",
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							return id
+								.toString()
+								.split("node_modules/")
+								[indexOne].split("/")
+								[indexZero].toString();
+						}
+					},
+				},
+			},
+		},
+		plugins: [react(), svgr()],
+		resolve: {
+			alias: [
+				{
+					find: "~",
+					replacement: fileURLToPath(new URL("src", import.meta.url)),
+				},
+			],
+		},
+		server: {
+			host: true,
+			port: Number(VITE_APP_DEVELOPMENT_PORT),
+		},
+	});
+};
+
+export default config;
