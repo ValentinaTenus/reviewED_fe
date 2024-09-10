a635157b39c0bda7fd5c66b6672379803f724f3b
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-10 13:37:27 +0200

    added helpers for create dropdown options format

---
+++ src/common/components/search-element/helpers/index.ts
@@ -0,0 +1,2 @@
+export { getDropdownOptionsFormat } from './get-dropdown-options-format';
+export { mapCompanies } from './map-companies';
---
+++ src/common/components/search-element/helpers/map-companies.ts
@@ -0,0 +1,22 @@
+import { type DropdownOption, type Company } from "~/common/types";
+
+const mapCompanies = (companies: Company[]) => {
+  const sortedCompanies = [...companies]
+  .sort((company1, company2) => +company2.average_rating - +company1.average_rating)
+  .map((company) => ({
+    value: company.id,
+    label: company.name
+  }));
+
+  const topCompanies = sortedCompanies.slice(0, 5);
+  const otherCompanies = sortedCompanies.slice(5);
+
+  const options: DropdownOption[] = [
+    {value: 1, label: 'Топ компанії', options: topCompanies},
+    {value: 2, label: 'Інші компанії', options: otherCompanies}
+  ];
+
+  return options
+}
+
+export { mapCompanies };
