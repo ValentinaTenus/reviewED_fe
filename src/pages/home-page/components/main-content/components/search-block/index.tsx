1ab7064f9e10aa5e57e61c5a60bef75e735ff1c5
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-11 01:20:30 +0200

    added not found component

--- src/pages/home-page/components/main-content/components/search-block/index.tsx
+++ src/pages/home-page/components/main-content/components/search-block/index.tsx
@@ -1,13 +1,22 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 
-import styles from './styles.module.scss';
+import { type Company, type Course } from '~/common/types/index';
+
+import { NotFound } from './components/index';
 import { SearchElement } from './components/index';
+import styles from './styles.module.scss';
 
 const SearchBlock: FC = () => {
+  const [searchResult, setSearchResult] = useState<Company[] | Course[]>([]);
+
+  const handleSearch = useCallback((searchResult: Company[] | Course[]) => {
+    setSearchResult(searchResult)
+  }, []);
 
   return (
     <div className={styles['search_block']}>
-      <SearchElement />
+      <SearchElement onSearch={handleSearch}/>
+      {searchResult.length === 0 && <NotFound />}
     </div>
   )
 };
