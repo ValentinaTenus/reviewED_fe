b98c88517875356d5c019ecaba85dba484772d52
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-10 13:35:30 +0200

    add types, constant

---
+++ src/common/types/company/get-companies-request-query.type.ts
@@ -0,0 +1,6 @@
+type GetCompaniesRequestQuery = {
+	name?: string;
+	city?: string;
+};
+
+export { type GetCompaniesRequestQuery };
--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,3 +1,7 @@
-export { type Company } from './company/index';
+export { 
+  type Company, 
+  type GetCompaniesRequestQuery, 
+  type GetCompaniesResponse 
+} from './company/index';
 export { type DropdownOption } from './dropdown-option';
 export { type ValueOf } from './value-of/index';
