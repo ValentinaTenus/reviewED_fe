6bb7be17eb9094a068da374c1183d20a1afb5d4a
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-11 15:32:13 +0200

    added button variant and icons

--- src/common/components/icon/icon-name-to-svg.ts
+++ src/common/components/icon/icon-name-to-svg.ts
@@ -1,4 +1,7 @@
 import SearchIcon from '~/assets/images/search-normal.svg?react';
+import ArrowRight from '~/assets/images/arrow-right.svg?react';
+import ArrowDown from '~/assets/images/arrow-down.svg?react';
+import ArrowUp from '~/assets/images/arrow-up.svg?react';
 import { type IconName } from '~/common/enums/index';
 
 const iconNameToSvg: Record<
@@ -6,6 +9,9 @@ const iconNameToSvg: Record<
 	React.FC<React.SVGProps<SVGSVGElement>>
 > = {
 	search: SearchIcon,
+	arrowDown: ArrowDown,
+	arrowRight: ArrowRight,
+	arrowUp: ArrowUp,
 };
 
 export { iconNameToSvg };
--- src/common/enums/icon-name.enum.ts
+++ src/common/enums/icon-name.enum.ts
@@ -1,5 +1,8 @@
 enum IconName {
   SEARCH = 'search',
+  ARROW_TOP = 'arrowUp',
+  ARROW_DOWN = 'arrowDown',
+  ARROW_RIGHT = 'arrowRight'
 }
 
 export { IconName };
