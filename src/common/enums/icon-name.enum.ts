6df539481aeca5f4492937baa56703cbf730c820
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-12 22:42:57 +0200

    added icons, variables

--- src/common/enums/icon-name.enum.ts
+++ src/common/enums/icon-name.enum.ts
@@ -3,8 +3,14 @@ enum IconName {
   ARROW_TOP = 'arrowUp',
   ARROW_DOWN = 'arrowDown',
   ARROW_RIGHT = 'arrowRight',
+  FACEBOOK = 'facebook',
+  EMAIL = 'email',
   MINUS = 'minus',
-  PLUS = 'plus'
+  LINKEDIN = 'linkedin',
+  PHONE = 'phone',
+  PLUS = 'plus',
+  YOUTUBE= 'youtube',
+  XRP = 'xrp'
 }
 
 export { IconName };
