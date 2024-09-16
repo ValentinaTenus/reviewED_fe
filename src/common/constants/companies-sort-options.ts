8c4c8d9da7d94ab89de96efd2bd6584be4858e31
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-16 15:16:50 +0200

    changed constants

---
+++ src/common/constants/companies-sort-options.ts
@@ -0,0 +1,16 @@
+const CompaniesSortOptions = [
+	{
+		label: "Найпопулярніші",
+		value: "",
+	},
+	{
+		label: "Найновіші",
+		value: "",
+	},
+	{
+		label: "Найнадійніші",
+		value: "-avg_rating",
+	},
+];
+
+export { CompaniesSortOptions };
