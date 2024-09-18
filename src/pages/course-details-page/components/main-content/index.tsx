2b73401ab32077f3d01494a9943d2f5133e4f466
Yuriy Mamoshin <yuriymamoshin@gmail.com>
2024-09-18 17:01:23 +0300

    created basic page structure, added it to router

---
+++ src/pages/course-details-page/components/main-content/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainContent: React.FC = () => {
+	return <div>Our new awesome main will be here soon</div>;
+};
+
+export { MainContent };
---
+++ src/pages/course-details-page/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Footer, Header } from "~/common/components";
+import styles from "./styles.module.scss";
+import { MainContent } from "./components";
+
+const CourseDetailsPage: React.FC = () => {
+	return (
+		<div className={styles["course_details_page"]}>
+			<Header />
+<MainContent />
+			<Footer />
+		</div>
+	);
+};
+
+export { CourseDetailsPage };
