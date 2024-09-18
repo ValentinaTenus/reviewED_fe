d31d4fcb06161faccd9dea13b721b0bc2d886e6d
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-19 01:08:14 +0200

    added reviews section

---
+++ src/common/constants/mock-data/mock-user-reviews.ts
@@ -0,0 +1,34 @@
+const UserReviews = [
+	{
+		id: 1,
+		logo: "",
+		rating: 5,
+		related_entity_name: "Course 1",
+		short_description: "It's was great course",
+		status: "",
+		time_added: "12/08/2024",
+		user_name: "User",
+	},
+	{
+		id: 2,
+		logo: "",
+		rating: 5,
+		related_entity_name: "Course 1",
+		short_description: "It's was great course",
+		status: "",
+		time_added: "12/08/2024",
+		user_name: "User",
+	},
+	{
+		id: 3,
+		logo: "",
+		rating: 5,
+		related_entity_name: "Course 1",
+		short_description: "It's was great course",
+		status: "",
+		time_added: "12/08/2024",
+		user_name: "User",
+	},
+];
+
+export { UserReviews };
---
+++ src/common/types/review/index.ts
@@ -0,0 +1,1 @@
+export { type GetCompanyAndCourseReviewsByUserIdResponse } from "./get-company-and-course-review-by-user-response-dto.type";
