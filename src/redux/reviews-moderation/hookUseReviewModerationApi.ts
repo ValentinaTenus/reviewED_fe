d2cc012c0c496792414099f2de380e56bd297599
KirillSerg <kirill.sergienko1989@gmail.com>
2024-09-27 08:32:50 +0300

    feat: finished filter section, added filter by type; added(duplicate) togglebottonsgroup

--- src/redux/reviews-moderation/hookUseReviewModerationApi.ts
+++ src/redux/reviews-moderation/hookUseReviewModerationApi.ts
@@ -12,42 +12,46 @@ type Params = {
 	type: GetModerationReviewsRequest["type"];
 };
 const useReviewModerationApi = (filters: Params) => {
+	const {
+		data: moderatorsReviewById,
+		error: errorById,
+		isFetching: isFetchingById,
+	} = useGetReviewsModerationByIdQuery(
+		{
+			id: filters.id,
+			type: filters.type,
+		},
+		{
+			refetchOnMountOrArgChange: true,
+		},
+	);
+	const {
+		data: filteredModeratorsReviews,
+		error: errorByFilter,
+		isFetching: isFetchingByFilter,
+	} = useGetReviewsModerationByFilterQuery(
+		{
+			id: filters.id,
+			ordering: filters.ordering,
+			status: filters.status,
+			type: filters.type,
+		},
+		{
+			refetchOnMountOrArgChange: true,
+		},
+	);
 	if (filters.id) {
-		const {
-			data: moderatorsReviewById,
-			error,
-			isFetching,
-		} = useGetReviewsModerationByIdQuery(
-			{
-				id: filters.id,
-				type: filters.type,
-			},
-			{
-				refetchOnMountOrArgChange: true,
-			},
-		);
 		return {
-			error,
-			isFetching,
+			error: errorById,
+			isFetching: isFetchingById,
 			reviews: moderatorsReviewById ? [moderatorsReviewById] : undefined,
 		};
 	} else {
-		const {
-			data: filteredModeratorsReviews,
-			error,
-			isFetching,
-		} = useGetReviewsModerationByFilterQuery(
-			{
-				id: filters.id,
-				ordering: filters.ordering,
-				status: filters.status,
-				type: filters.type,
-			},
-			{
-				refetchOnMountOrArgChange: true,
-			},
-		);
-		return { error, isFetching, reviews: filteredModeratorsReviews?.results };
+		return {
+			error: errorByFilter,
+			isFetching: isFetchingByFilter,
+			reviews: filteredModeratorsReviews?.results,
+		};
 	}
 };
 export default useReviewModerationApi;
