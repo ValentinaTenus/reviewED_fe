0f05ffa16724e1f6e8c60d26c225c4f43ec9c68b
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-09-30 20:17:09 +0200

    add modal as reusable component

--- src/pages/course-list-page/components/course-list/components/filter-section/components/filter-modal/index.tsx
+++ src/pages/course-list-page/components/course-list/components/filter-section/components/filter-modal/index.tsx
@@ -1,13 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 
-import {
-	Button,
-	CheckDropdown,
-	Icon,
-	IconButton,
-} from "~/common/components/index";
-import { ButtonVariant, IconName } from "~/common/enums/index";
-import { useModal } from "~/common/hooks/index";
+import { Button, CheckDropdown, Modal } from "~/common/components/index";
+import { ButtonVariant } from "~/common/enums/index";
 import { DropdownOption, FilterType } from "~/common/types/index";
 import { mapCoursesCategories } from "~/pages/home-page/components/main-content/components/search-block/helpers";
 import { useGetCategoriesQuery } from "~/redux/categories/categories-api";
@@ -46,11 +40,6 @@ const FilterModal: React.FC<FilterModalProperties> = ({
 	const coursesCategoriesOptions = mapCoursesCategories(
 		fetchedCategories || [],
 	);
-
-	const { handleOutsideClick, preventModalCloseOnClick } = useModal({
-		isOpen,
-		onClose,
-	});
 
 	useEffect(() => {
 		if (locations) {
@@ -100,58 +89,49 @@ const FilterModal: React.FC<FilterModalProperties> = ({
 		return null;
 	}
 
-	return (
-		<div className={styles["modal"]} onClick={handleOutsideClick}>
-			<div
-				className={styles["modal__container"]}
-				onClick={preventModalCloseOnClick}
-				tabIndex={-1}
+	const modalFooter = (
+		<>
+			<Button
+				className={styles["modal__clear_button"]}
+				onClick={onClearFilters}
+				variant={ButtonVariant.DEFAULT}
 			>
-				<div className={styles["modal__header"]}>
-					<h2 className={styles["modal__title"]}>Фільтр за</h2>
-					<IconButton onClick={onClose}>
-						<Icon
-							className={styles["modal__icon-close"]}
-							name={IconName.CLOSE}
-						/>
-					</IconButton>
-				</div>
-				<div className={styles["modal__filters_content"]}>
-					<CheckDropdown
-						className={styles["search_dropdown"]}
-						name="Види курсів"
-						onChange={handleSelectSubCategory}
-						options={coursesCategoriesOptions.slice(OPTIONS_FIRST_ITEM_INDEX)}
-						placeholder="Види курсів"
-						selectedItems={selectedSubCategories}
-					/>
-					<CheckDropdown
-						className={styles["search_dropdown"]}
-						name="Локації"
-						onChange={handleSelectLocation}
-						options={locationOptions}
-						placeholder="Локації"
-						selectedItems={selectedLocations}
-					/>
-				</div>
-				<div className={styles["modal__buttons_container"]}>
-					<Button
-						className={styles["modal__clear_button"]}
-						onClick={onClearFilters}
-						variant={ButtonVariant.DEFAULT}
-					>
-						Очистити все
-					</Button>
-					<Button
-						className={styles["modal__clear_button"]}
-						onClick={handleApplyFilters}
-						variant={ButtonVariant.PRIMARY}
-					>
-						Застосувати
-					</Button>
-				</div>
-			</div>
-		</div>
+				Очистити все
+			</Button>
+			<Button
+				className={styles["modal__clear_button"]}
+				onClick={handleApplyFilters}
+				variant={ButtonVariant.PRIMARY}
+			>
+				Застосувати
+			</Button>
+		</>
+	);
+
+	return (
+		<Modal
+			footer={modalFooter}
+			isOpen={isOpen}
+			onClose={onClose}
+			title="Фільтр за"
+		>
+			<CheckDropdown
+				className={styles["search_dropdown"]}
+				name="Види курсів"
+				onChange={handleSelectSubCategory}
+				options={coursesCategoriesOptions.slice(OPTIONS_FIRST_ITEM_INDEX)}
+				placeholder="Види курсів"
+				selectedItems={selectedSubCategories}
+			/>
+			<CheckDropdown
+				className={styles["search_dropdown"]}
+				name="Локації"
+				onChange={handleSelectLocation}
+				options={locationOptions}
+				placeholder="Локації"
+				selectedItems={selectedLocations}
+			/>
+		</Modal>
 	);
 };
 
