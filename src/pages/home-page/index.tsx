c973ec30b32953eb953235cd761637ea00dfc4a1
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-10-04 22:03:14 +0200

    delete modal from home page

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Footer, Header } from "~/common/components/index";
-import { PrivacyPolicyModal } from "~/common/components/privacy-policy-modal";
 
 import { MainContent } from "./components/index";
 import styles from "./styles.module.scss";
 
 const HomePage: React.FC = () => {
-	const [isOpen, setIsOpen] = useState(false);
-
 	return (
 		<div className={styles["home_page"]}>
 			<Header />
-			{isOpen && <PrivacyPolicyModal isOpen onClose={() => setIsOpen(false)} />}
 			<MainContent />
 			<Footer />
 		</div>
