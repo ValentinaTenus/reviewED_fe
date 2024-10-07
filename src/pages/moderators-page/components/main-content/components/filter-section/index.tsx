46076209df56dba3a32fd35c66eafb603c622489
KirillSerg <kirill.sergienko1989@gmail.com>
2024-10-07 20:45:49 +0300

    refact: moved the state of sort filters from useState to useSearchParams to save their values between modals

--- src/pages/moderators-page/components/main-content/components/filter-section/index.tsx
+++ src/pages/moderators-page/components/main-content/components/filter-section/index.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 import CloseCircle from "~/assets/images/close-circle.svg?react";
 import { Button, SortDropdown, ToggleGroupButtons } from "~/common/components";
@@ -8,34 +9,38 @@ import {
 	moderationsReviewSortOptionsByStatus,
 	ScreenBreakpoints,
 } from "~/common/constants";
-import { ButtonGroupData, ButtonVariant } from "~/common/enums";
+import { ButtonVariant } from "~/common/enums";
 import { useGetScreenWidth } from "~/common/hooks";
-import { DropdownOption } from "~/common/types";
 
 import styles from "./styles.module.scss";
 
-type Props = {
-	filterByType: keyof typeof ButtonGroupData;
-	handleSetFilterByStatus: (sortOption: DropdownOption["value"]) => void;
-	handleSetSortByPeriod: (sortOption: DropdownOption["value"]) => void;
-	isActiveGroupCleaner?: boolean;
-	setFilterByType: (
-		value: React.SetStateAction<keyof typeof ButtonGroupData>,
-	) => void;
+const initialSearchParamsObj = {
+	ordering: "",
+	status: "",
+	type: "",
 };
 
-const ModeratorsReviewFilterSection: React.FC<Props> = ({
-	filterByType,
-	handleSetFilterByStatus,
-	handleSetSortByPeriod,
-	isActiveGroupCleaner,
-	setFilterByType,
-}) => {
+const ModeratorsReviewFilterSection: React.FC = () => {
 	const [isClean, setIsClean] = useState(false);
+	const [searchParams, setSearchParams] = useSearchParams(
+		initialSearchParamsObj,
+	);
+	const filterByType = searchParams.get("type");
+	const filterByStatus = searchParams.get("status");
+	const sortByPeriod = searchParams.get("ordering");
+
+	const handleSetFilterByType = useCallback(
+		(type: string) => {
+			setSearchParams((prev) => {
+				prev.set("type", type);
+				return prev;
+			});
+		},
+		[setSearchParams],
+	);
 
 	const handleCleanSortGroup = () => {
-		handleSetFilterByStatus("");
-		handleSetSortByPeriod("");
+		setSearchParams(initialSearchParamsObj);
 		setIsClean(true);
 	};
 
@@ -50,10 +55,8 @@ const ModeratorsReviewFilterSection: React.FC<Props> = ({
 					Оберіть категорію
 				</p>
 				<ToggleGroupButtons
-					activeButtonValue={filterByType}
-					handleButtonClick={(type) =>
-						setFilterByType(type as keyof typeof ButtonGroupData)
-					}
+					activeButtonValue={filterByType || "Компанії"}
+					handleButtonClick={handleSetFilterByType}
 					toggleButtonGroupData={["Компанії", "Курси"]}
 				/>
 			</div>
@@ -64,9 +67,9 @@ const ModeratorsReviewFilterSection: React.FC<Props> = ({
 					isClean={isClean}
 					isWithCleaner
 					menuStaticStyle={useGetScreenWidth() <= ScreenBreakpoints.MOBILE}
-					onChange={handleSetFilterByStatus}
 					onSetIsClean={handleSetIsClean}
 					options={moderationsReviewSortOptionsByStatus}
+					searchParam="status"
 					title="Стан перевірки"
 				/>
 				<SortDropdown
@@ -74,16 +77,17 @@ const ModeratorsReviewFilterSection: React.FC<Props> = ({
 					isClean={isClean}
 					isWithCleaner
 					menuStaticStyle={useGetScreenWidth() <= ScreenBreakpoints.MOBILE}
-					onChange={handleSetSortByPeriod}
 					onSetIsClean={handleSetIsClean}
 					options={moderationsReviewSortOptionsByPeriod}
+					searchParam="ordering"
 					title="Період"
 				/>
 
 				<Button
 					className={clsx(
 						styles["sort-group_cleaner-btn"],
-						isActiveGroupCleaner && styles["sort-group_cleaner-btn__active"],
+						(filterByStatus || sortByPeriod) &&
+							styles["sort-group_cleaner-btn__active"],
 					)}
 					onClick={handleCleanSortGroup}
 					prependedIcon={<CloseCircle />}
