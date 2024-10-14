3e42c27a617cb1307bf70718dd5b458cd8ed9cf4
Yuriy Mamoshin <yuriymamoshin@gmail.com>
2024-10-14 11:49:24 +0300

    added and adjusted pagination

--- src/pages/course-details-page/components/main-content/components/reviews-bar/components/reviews-list/index.tsx
+++ src/pages/course-details-page/components/main-content/components/reviews-bar/components/reviews-list/index.tsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { type CourseReview } from "~/common/types";
 
 import { Review } from "./components/review";
 import styles from "./styles.module.scss";
+import { Pagination } from "~/common/components";
 
 type ReviewListProperties = {
 	reviews: CourseReview[];
 };
 
+const MAX_REVIEWS_SHOWN = 4;
+const DEFAULT_PAGE = 1;
+
 const ReviewsList: React.FC<ReviewListProperties> = ({ reviews }) => {
+	const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
+
+	const isPaginationNeeded = reviews.length > MAX_REVIEWS_SHOWN;
+
+	const firstReviewIndex = (currentPage - 1) * MAX_REVIEWS_SHOWN;
+	const lastReviewIndex = firstReviewIndex + MAX_REVIEWS_SHOWN;
+
+	let pagesNumber = 0;
+
+	if (reviews) {
+		pagesNumber = Math.ceil(reviews.length / MAX_REVIEWS_SHOWN);
+	}
+
 	return (
 		<div className={styles["reviews-list"]}>
-			{reviews.map((review, index) => (
+			{(isPaginationNeeded
+				? reviews.slice(firstReviewIndex, lastReviewIndex)
+				: reviews
+			).map((review, index) => (
 				<Review key={index} review={review} />
 			))}
+
+			{isPaginationNeeded && (
+				<Pagination
+					defaultCurrentPage={currentPage}
+					pages={pagesNumber}
+					setCurrentPage={setCurrentPage}
+				/>
+			)}
 		</div>
 	);
 };
