ce88774dede52a4681c95d2b43e332ebf098c28f
AndriySt92 <andriyua-1992@mail.ru>
2024-10-23 11:37:57 +0300

    feat: add ReviewStatus component

---
+++ src/pages/my-reviews-page/components/main-content/components/my-reviews-list-section/components/review-status/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import clsx from "clsx";
+
+const STATUS_TRANSLATIONS = {
+	pending: "В очікуванні",
+	published: "Опубліковано",
+	removed: "Неуспішно",
+};
+
+type Status = keyof typeof STATUS_TRANSLATIONS;
+
+type Properties = {
+	status: Status;
+};
+
+const ReviewStatus: React.FC<Properties> = ({ status }) => {
+	return (
+		<div className={styles["status"]}>
+			<span className={clsx(styles["status__text"], styles[`${status}`])}>
+				{STATUS_TRANSLATIONS[status]}
+			</span>
+		</div>
+	);
+};
+
+export { ReviewStatus };
