cc592a3563791cc82f15bacbbf00386b430002a2
AndriySt92 <andriyua-1992@mail.ru>
2024-10-28 18:07:29 +0200

    feat: add MoreOptions component

--- src/pages/my-reviews-page/components/main-content/components/my-reviews-list-section/components/index.ts
+++ src/pages/my-reviews-page/components/main-content/components/my-reviews-list-section/components/index.ts
@@ -4,6 +4,7 @@ export { DialogModal } from "./dialog-modal/index";
 export { EditReviewModal } from "./edit-review-modal/index";
 export { HeaderList } from "./header-list/index";
 export { IconsSection } from "./icons-section/index";
+export { MoreOptions } from "./more-options/index";
 export { MyReviewsList } from "./my-reviews-list/index";
 export { NotFound } from "./not-found/index";
 export { PopupMenu } from "./popup-menu/index";
---
+++ src/pages/my-reviews-page/components/main-content/components/my-reviews-list-section/components/more-options/index.tsx
@@ -0,0 +1,114 @@
+import clsx from "clsx";
+import React, { useCallback, useState } from "react";
+
+import { Icon } from "~/common/components";
+import { IconName } from "~/common/enums";
+import { MyReviewOptions } from "~/common/types/my-reviews";
+
+import { ActionsReviewModal } from "../actions-review-modal";
+import { PopupMenu } from "../popup-menu";
+import styles from "./styles.module.scss";
+
+const MY_REVIEW_OPTIONS: MyReviewOptions[] = [
+	{ iconName: IconName.EDIT, label: "редагувати", value: "edit" },
+	{
+		iconName: IconName.MESSAGES,
+		label: "зв'язатися з модератором",
+		value: "contact moderator",
+	},
+	{ iconName: IconName.DELETE, label: "видалити", value: "delete" },
+];
+
+const POPUP_CLOSE_DELAY = 500;
+const MOBILE_BREAKPOINT = 576;
+
+type Properties = {
+	handleClickDeleteReview: (entityId: null | number) => void;
+	handleClickEditReview: (entityId: null | number) => void;
+	isForTablet?: boolean;
+	reviewId: null | number;
+};
+
+const MoreOptions: React.FC<Properties> = ({
+	handleClickDeleteReview,
+	handleClickEditReview,
+	isForTablet = false,
+	reviewId,
+}) => {
+	const [isOpenActionsModal, setIsOpenActionsModal] = useState<boolean>(false);
+	const [isOpenPopup, setIsOpenPopup] = useState<boolean>(false);
+	const [activePopup, setActivePopup] = useState<null | number>(null);
+
+	const handleClosePopup = useCallback(() => {
+		setActivePopup(null);
+		// Prevent reopening the Popup if the outside click is on the Popup open trigger
+		setTimeout(() => {
+			setIsOpenPopup(false);
+		}, POPUP_CLOSE_DELAY);
+	}, []);
+
+	const handleOpenPopup = useCallback(() => {
+		const width = window.innerWidth;
+
+		// for mobile open ActionsModal, for desktop and tablet open PopupMenu
+		if (width > MOBILE_BREAKPOINT) {
+			setIsOpenPopup(true);
+			setActivePopup(reviewId);
+		} else {
+			setIsOpenActionsModal(true);
+		}
+	}, [reviewId]);
+
+	const handleSelectOption = useCallback(
+		(option: string) => {
+			if (option === "edit") {
+				handleClickEditReview(reviewId);
+			}
+
+			if (option === "delete") {
+				handleClickDeleteReview(reviewId);
+			}
+
+			handleClosePopup();
+		},
+		[
+			handleClickDeleteReview,
+			handleClickEditReview,
+			handleClosePopup,
+			reviewId,
+		],
+	);
+
+	return (
+		<div
+			className={clsx(styles["more-options"], {
+				[styles["more-options--tablet"]]: isForTablet,
+			})}
+			onClick={!isOpenPopup ? handleOpenPopup : undefined}
+		>
+			<span className={styles["more-options__button"]}>
+				<Icon name={IconName.MORE} />
+			</span>
+
+			<div className={styles["more-options__popup-menu"]}>
+				<PopupMenu
+					handleClosePopup={handleClosePopup}
+					handleSelect={handleSelectOption}
+					isOpen={activePopup === reviewId}
+					options={MY_REVIEW_OPTIONS}
+				/>
+			</div>
+
+			{isOpenActionsModal && (
+				<ActionsReviewModal
+					isOpen={isOpenActionsModal}
+					onSelect={handleSelectOption}
+					options={MY_REVIEW_OPTIONS}
+					setIsOpenActionsModal={setIsOpenActionsModal}
+				/>
+			)}
+		</div>
+	);
+};
+
+export { MoreOptions };
