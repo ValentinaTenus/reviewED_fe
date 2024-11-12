887f3d46ed0054c8b9e2aaa947cd8ccea62b5aed
AndriySt92 <andriyua-1992@mail.ru>
2024-11-12 16:16:13 +0200

    refactor: change way of text truncation in ReviewTextSection component. Fix modals components styles

--- src/pages/my-reviews-page/components/main-content/components/my-reviews-list-section/components/review-text-section/index.tsx
+++ src/pages/my-reviews-page/components/main-content/components/my-reviews-list-section/components/review-text-section/index.tsx
@@ -1,20 +1,10 @@
-import React, { useCallback, useEffect, useState } from "react";
+import clsx from "clsx";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import { ActionIconsPanel } from "../index";
 import styles from "./styles.module.scss";
 
-// Constants for breakpoints and max preview lengths
-const MAX_PREVIEW_LENGTH_DESKTOP = 200;
-const MAX_PREVIEW_LENGTH_MOBILE = 160;
-const MAX_PREVIEW_LENGTH_TABLET = 290;
-const MAX_PREVIEW_LENGTH_LARGE_TABLET = 440;
-const MAX_PREVIEW_LENGTH_LARGE_SCREEN = 190;
-const MAX_PREVIEW_LENGTH_EXTRA_LARGE = 205;
-const MOBILE_BREAKPOINT = 576;
-const TABLET_BREAKPOINT = 630;
-const LARGE_TABLET_BREAKPOINT = 888;
-const LARGE_SCREEN_BREAKPOINT = 1150;
-const ZERO_NUMBER = 0;
+const COLLAPSED_TEXT_HEIGHT = 80;
 
 type Properties = {
 	id: number;
@@ -30,84 +20,72 @@ const ReviewTextSection: React.FC<Properties> = ({
 	text,
 }) => {
 	const [showFullText, setShowFullText] = useState(false);
-	const [maxPreviewLength, setMaxPreviewLength] = useState(
-		MAX_PREVIEW_LENGTH_DESKTOP,
-	);
+	const [isTextTruncated, setIsTextTruncated] = useState(false);
 
-	const toggleText = useCallback(
-		() => setShowFullText(!showFullText),
-		[showFullText],
-	);
+	const textRef = useRef<HTMLDivElement>(null);
 
-	const handleResize = useCallback(() => {
-		const width = window.innerWidth;
+	const toggleText = useCallback(() => {
+		setShowFullText(!showFullText);
+	}, [showFullText]);
 
-		if (width <= MOBILE_BREAKPOINT) {
-			setMaxPreviewLength(MAX_PREVIEW_LENGTH_MOBILE);
-		} else if (width <= TABLET_BREAKPOINT) {
-			setMaxPreviewLength(MAX_PREVIEW_LENGTH_TABLET);
-		} else if (width <= LARGE_TABLET_BREAKPOINT) {
-			setMaxPreviewLength(MAX_PREVIEW_LENGTH_LARGE_TABLET);
-		} else if (width <= LARGE_SCREEN_BREAKPOINT) {
-			setMaxPreviewLength(MAX_PREVIEW_LENGTH_LARGE_SCREEN);
-		} else {
-			setMaxPreviewLength(MAX_PREVIEW_LENGTH_EXTRA_LARGE);
+	const checkTextOverflow = useCallback(() => {
+		if (textRef.current) {
+			const isOverflowing =
+				textRef.current.scrollHeight > COLLAPSED_TEXT_HEIGHT;
+
+			if (!isOverflowing) {
+				setShowFullText(false);
+			}
+
+			setIsTextTruncated(isOverflowing);
 		}
 	}, []);
 
 	useEffect(() => {
-		handleResize();
+		checkTextOverflow();
+	}, [text, checkTextOverflow]);
 
-		window.addEventListener("resize", handleResize);
-
+	useEffect(() => {
+		window.addEventListener("resize", checkTextOverflow);
 		return () => {
-			window.removeEventListener("resize", handleResize);
+			window.removeEventListener("resize", checkTextOverflow);
 		};
-	}, [handleResize]);
+	}, [checkTextOverflow]);
 
 	return (
 		<div className={styles["review-text"]}>
-			{showFullText ? (
-				<>
-					<span className={styles["review-text-content"]}>{text}</span>
-					<p className={styles["review-text__close"]} onClick={toggleText}>
-						Закрити
-					</p>
-					<div className={styles["review-text__icons"]}>
-						<ActionIconsPanel
-							likesCount={likesCount}
-							openModal={openModal}
-							reviewId={id}
-							showEditIcon
-						/>
-					</div>
-				</>
-			) : (
-				<>
-					{text.length > maxPreviewLength ? (
-						<>
-							{text.slice(ZERO_NUMBER, maxPreviewLength)}
-							<span
-								className={styles["review-text__more-details"]}
-								onClick={toggleText}
-							>
-								... Детальніше
-							</span>
-						</>
-					) : (
-						<>
-							<span className={styles["review-text__full-text"]}>{text}</span>
-							<div className={styles["review-text__full-text-icons"]}>
-								<ActionIconsPanel
-									likesCount={likesCount}
-									openModal={openModal}
-									reviewId={id}
-									showEditIcon
-								/>
-							</div>
-						</>
-					)}
-				</>
+			<div
+				className={clsx(styles["review-text__text"], {
+					[styles["review-text__text--full"]]: showFullText,
+				})}
+				ref={textRef}
+			>
+				{text}
+			</div>
+			{!showFullText && isTextTruncated && (
+				<span
+					className={styles["review-text__more-details"]}
+					onClick={toggleText}
+				>
+					<span>...</span> Детальніше
+				</span>
+			)}
+
+			{showFullText && (
+				<span className={styles["review-text__close"]} onClick={toggleText}>
+					Закрити
+				</span>
+			)}
+
+			{(showFullText || !isTextTruncated) && (
+				<div className={styles["review-text__full-text-icons"]}>
+					<ActionIconsPanel
+						likesCount={likesCount}
+						openModal={openModal}
+						reviewId={id}
+						showEditIcon
+					/>
+				</div>
 			)}
 		</div>
 	);
